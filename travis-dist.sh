1e619fc9573655bc84f9dfa7353d10a8d8dc089c
Kiril Vatev <vatev.1@gmail.com>
2019-11-26 18:35:53 -0500

    adding script to create dist files

---
+++ travis-dist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echoerr() { echo "$@" 1>&2; }
+
+function assertFile {
+  if [ ! -f "$1" ]; then
+    echoerr "$1" does not exist
+    exit 1
+  fi
+}
+
+mkdir -p dist
+cp libheif/libheif.js dist/libheif.js
+cp libheif/COPYING dist/LICENSE
+
+assertFile dist/libheif.js
+assertFile dist/LICENSE
