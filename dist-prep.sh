70b96c27a319bae6aea9754d119a8fa6673a714d
Kiril Vatev <vatev.1@gmail.com>
2023-11-02 15:10:17 -0400

    outputting separate artifacts so that I can see all files

--- dist-prep.sh
+++ dist-prep.sh
@@ -17,9 +17,11 @@ ls -la libheif
 
 cp libheif/libheif.js dist/libheif.js
 cp libheif/COPYING dist/LICENSE
+cp libheif/libheif.wasm dist/libheif.wasm || echo wasm not found
 
 assertFile dist/libheif.js
 assertFile dist/LICENSE
 
 chown $(whoami) dist/libheif.js dist/LICENSE
+chown $(whoami) dist/libheif.wasm || echo wasm not found
 ls -la dist
