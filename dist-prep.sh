f506f1eab1df224c30f8fb80ef21581a471115cc
Kiril Vatev <vatev.1@gmail.com>
2023-11-03 00:30:24 -0400

    making sure to copy all necessary files to dist based on known build target

--- dist-prep.sh
+++ dist-prep.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+build_target="$1"
+
 echoerr() { echo "$@" 1>&2; }
 
 function assertFile {
@@ -15,13 +17,33 @@ rm -rf dist/*
 
 ls -la libheif
 
-cp libheif/libheif.js dist/libheif.js
-cp libheif/COPYING dist/LICENSE
-cp libheif/libheif.wasm dist/libheif.wasm || echo wasm not found
+function copyJs() {
+  cp libheif/libheif.js dist/libheif.js
+  cp libheif/COPYING dist/LICENSE
 
-assertFile dist/libheif.js
-assertFile dist/LICENSE
+  assertFile dist/libheif.js
+  assertFile dist/LICENSE
 
-chown $(whoami) dist/libheif.js dist/LICENSE
-chown $(whoami) dist/libheif.wasm || echo wasm not found
+  chown $(whoami) dist/libheif.js dist/LICENSE
+}
+
+function copyWasm() {
+  copyJs
+
+  cp libheif/libheif.wasm dist/libheif.wasm
+  assertFile dist/libheif.wasm
+  chown $(whoami) dist/libheif.wasm
+}
+
+if [ "$build_target" = "js" ]
+then
+  copyJs
+elif [ "$build_target" = "wasm" ]
+then
+  copyWasm
+else
+  echo "unknown build target: $build_target"
+  exit 1
+fi
+
 ls -la dist
